522634af3f7f79ba9e756c80657ec16ea53afd00
zarbabi <z.arbabi4@gmail.com>
2021-10-22 13:52:18 +0330

    styling contactList component

--- src/components/ContactList/Contactlist.js
+++ src/components/ContactList/Contactlist.js
@@ -1,17 +1,25 @@
+import "./contactList.css";
+import userImage from "../../assets/images/user.png";
 const ContactList = ({ contacts, onDelete }) => {
   return (
-    <>
+    <section className="contactList">
       {contacts.map((contact) => {
         const { name, email, id } = contact;
         return (
-          <div key={id}>
-            <p>name:{name}</p>
-            <p>email:{email}</p>
+          <div key={id} className="item">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img src={userImage} alt="user" />
+              <div>
+                <p>name:{name}</p>
+                <p>email:{email}</p>
+              </div>
+            </div>
+
             <button onClick={() => onDelete(id)}>Delete</button>
           </div>
         );
       })}
-    </>
+    </section>
   );
 };
 
