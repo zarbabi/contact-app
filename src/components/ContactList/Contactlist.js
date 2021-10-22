e73125fc8109cea8f343a958775c8dbc0d770394
zarbabi <z.arbabi4@gmail.com>
2021-10-22 20:51:15 +0330

    imrpving some styles of application

--- src/components/ContactList/Contactlist.js
+++ src/components/ContactList/Contactlist.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import Contact from "./Contact/Contact";
 const ContactList = ({ contacts, onDelete }) => {
   return (
-    <section className="contactList">
-      <div>
-        <h2>Contacts</h2>
-        <Link to="/add">
-          <button>Add</button>
-        </Link>
+    <section className="listWrapper">
+      <div className="contactList">
+        <div className="listHeader">
+          <h2>Contacts</h2>
+          <Link to="/add">
+            <button>Add</button>
+          </Link>
+        </div>
+        {contacts.map((contact) => {
+          return <Contact contact={contact} onDelete={onDelete} />;
+        })}
       </div>
-      {contacts.map((contact) => {
-        return <Contact contact={contact} onDelete={onDelete}/>;
-      })}
     </section>
   );
 };
