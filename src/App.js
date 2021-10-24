fda69762534f2e16c14c4c98427a0f7a3a62d88c
zarbabi <z.arbabi4@gmail.com>
2021-10-24 17:06:00 +0330

    adding json sever DB to application

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddContact from "./components/AddContact/AddContact";
 import ContactList from "./components/ContactList/Contactlist";
 import { Switch, Route } from "react-router-dom";
 import ContactDetail from "./components/ContactDetail/ContactDetail";
+import axios from "axios";
 function App() {
   const [contacts, setContacts] = useState([]);
 
@@ -20,8 +21,13 @@ function App() {
   };
 
   useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
-    if (savedContacts) setContacts(savedContacts);
+    // const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    // if (savedContacts) setContacts(savedContacts);
+    const getContacts = async () => {
+      const { data } = await axios.get("http://localhost:3001/contacts");
+      setContacts(data); 
+    };
+    getContacts();
   }, []);
 
   useEffect(() => {
